3a2008731a797dcd31718a7b3f84a70e4ac7b811
wangzhang <745268436@qq.com>
2022-08-04 17:20:57 +0800

    update abstract factory

--- creational-pattern/abstract-factory/test/pipeline_test.go
+++ creational-pattern/abstract-factory/test/pipeline_test.go
@@ -1,12 +1,13 @@
-package abstract_factory
+package test
 
 import (
+	"github.com/go-summary/design-patterns/creational-pattern/abstract-factory"
 	"testing"
 )
 
 func TestPipeline(t *testing.T) {
 	// 其中pipeline.DefaultConfig()的配置内容见【抽象工厂模式示例图】
 	// 消息处理流程为 HelloInput -> UpperFilter -> ConsoleOutput
-	p := Of(DefaultConfig())
+	p := abstract_factory.Of(abstract_factory.DefaultConfig())
 	p.Exec()
 }
