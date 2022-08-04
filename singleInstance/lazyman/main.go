881b8b1e04a481268f71b4aa14503f2589e7a045
wangzhang <745268436@qq.com>
2022-08-04 10:24:48 +0800

    update single

--- singleInstance/lazyman/main.go
+++ singleInstance/lazyman/main.go
@@ -1,5 +1,37 @@
 package lazyman
 
-import "sync"
+import (
+	"github.com/go-summary/design-patterns/singleInstance/hungryman"
+	"sync"
+)
 
-var one = &sync.Once{}
+var once = &sync.Once{}
+
+// 消息池
+type messagePool struct {
+	pool *sync.Pool
+}
+
+var msgPool *messagePool
+
+func Instance() *messagePool {
+	once.Do(func() {
+		msgPool = &messagePool{
+			// 如果消息池里没有消息，则新建一个Count值为0的Message实例
+			pool: &sync.Pool{
+				New: func() any {
+					return &hungryman.Message{Count: 0}
+				},
+			},
+		}
+	})
+	return msgPool
+}
+
+func (m *messagePool) AddMsg(msg *hungryman.Message) {
+	m.pool.Put(msg)
+}
+
+func (m *messagePool) GetMsg() *hungryman.Message {
+	return m.pool.Get().(*hungryman.Message)
+}
---
+++ singleInstance/singleInstance_test.go
@@ -0,0 +1,33 @@
+package singleInstance
+
+import (
+	"github.com/go-summary/design-patterns/singleInstance/hungryman"
+	"github.com/go-summary/design-patterns/singleInstance/lazyman"
+	"testing"
+)
+
+func TestInstance0(t *testing.T) {
+	msg0 := hungryman.Instance().GetMsg()
+	if msg0.Count != 0 {
+		t.Errorf("expect msg count %d, but actual %d.", 0, msg0.Count)
+	}
+	msg0.Count = 1
+	hungryman.Instance().AddMsg(msg0)
+	msg1 := hungryman.Instance().GetMsg()
+	if msg1.Count != 1 {
+		t.Errorf("expect msg count %d, but actual %d.", 1, msg1.Count)
+	}
+}
+
+func TestInstance1(t *testing.T) {
+	msg0 := lazyman.Instance().GetMsg()
+	if msg0.Count != 0 {
+		t.Errorf("expect msg count %d, but actual %d.", 0, msg0.Count)
+	}
+	msg0.Count = 1
+	lazyman.Instance().AddMsg(msg0)
+	msg1 := lazyman.Instance().GetMsg()
+	if msg1.Count != 1 {
+		t.Errorf("expect msg count %d, but actual %d.", 1, msg1.Count)
+	}
+}
