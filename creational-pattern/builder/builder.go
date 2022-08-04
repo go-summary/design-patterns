32e173a5ef9840d9de01205575a51f4ff8bc1094
wangzhang <745268436@qq.com>
2022-08-04 11:53:05 +0800

    update builder

---
+++ creational-pattern/builder/builder.go
@@ -0,0 +1,73 @@
+package builder
+
+import "sync"
+
+// 针对对象成员较多，创建对象逻辑较为繁琐的场景
+// 适合使用建造者模式来进行优化
+
+// 造者模式的作用有如下几个：
+// 1、封装复杂对象的创建过程，使对象使用者不感知复杂的创建逻辑。
+// 2、可以一步步按照顺序对成员进行赋值，或者创建嵌套对象，并最终完成目标对象的创建。
+// 3、对多个对象复用同样的对象创建逻辑。
+
+type Message struct {
+	Header *Header
+	Body   *Body
+}
+type Header struct {
+	SrcAddr  string `json:"srcAddr, omitempty"`
+	SrcPort  uint64
+	DestAddr string
+	DestPort uint64
+	Items    map[string]string
+}
+type Body struct {
+	Items []string
+}
+
+// 建造者模式的方式
+type builder struct {
+	once *sync.Once
+	msg  *Message
+}
+
+func Builder() *builder {
+	return &builder{
+		once: &sync.Once{},
+		msg:  &Message{&Header{}, &Body{}},
+	}
+}
+
+// 成员构建
+func (b *builder) WithSrcPort(srcPort uint64) *builder {
+	b.msg.Header.SrcPort = srcPort
+	return b
+}
+func (b *builder) WithDestAddr(destAddr string) *builder {
+	b.msg.Header.DestAddr = destAddr
+	return b
+}
+func (b *builder) WithDestPort(destPort uint64) *builder {
+	b.msg.Header.DestPort = destPort
+	return b
+}
+func (b *builder) WithSrcAddr(srcAddr string) *builder {
+	b.msg.Header.SrcAddr = srcAddr
+	return b
+}
+func (b *builder) WithHeaderItem(key, value string) *builder {
+	b.once.Do(func() {
+		b.msg.Header.Items = map[string]string{}
+	})
+	b.msg.Header.Items[key] = value
+	return b
+}
+
+func (b *builder) WithBodyItem(record string) *builder {
+	b.msg.Body.Items = append(b.msg.Body.Items, record)
+	return b
+}
+
+func (b *builder) Build() *Message {
+	return b.msg
+}
---
+++ creational-pattern/builder/builder_test.go
@@ -0,0 +1,28 @@
+package builder
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestBuilder(t *testing.T) {
+	// 使用消息建造者进行对象创建
+	message := Builder().
+		WithSrcAddr("192.168.0.1").
+		WithSrcPort(8081).
+		WithDestAddr("192.168.0.2").
+		WithDestPort(8080).
+		WithHeaderItem("contents", "application/json").
+		WithBodyItem("record1").
+		WithBodyItem("record2").
+		Build()
+	if message.Header.SrcAddr != "192.168.0.1" {
+		t.Errorf("expect src address 192.168.0.1, but actual %s.", message.Header.SrcAddr)
+	}
+	if message.Body.Items[0] != "record1" {
+		t.Errorf("expect body item0 record1, but actual %s.", message.Body.Items[0])
+	}
+	v, _ := json.Marshal(&message)
+	fmt.Println(string(v))
+}
